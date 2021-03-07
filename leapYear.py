ad6a7b46276b2596a9c68e652e2a2f7258d5b9f4
Maddie Smith <smitmad9@oregonstate.edu>
2021-03-07 15:43:17 -0800

    Implemented including multiples of 400 for LYs

--- leapYear.py
+++ leapYear.py
@@ -5,8 +5,9 @@ def leapYear(n):
     # If the year is divisible by 4, it is a leap year
     if n % 4 == 0:
 
-        # The year must also NOT be divisible by 100 to be a leap year
-        if n % 100 != 0:
+        # The year must also NOT be divisible by 100 OR divisible by 400
+        # to be a leap year
+        if n % 100 != 0 or n % 400 == 0:
             return True
 
     return False
