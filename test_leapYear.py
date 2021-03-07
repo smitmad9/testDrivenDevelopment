9f6d581fc2dcd0a18ff85b85917eb965b473fdde
Maddie Smith <smitmad9@oregonstate.edu>
2021-03-07 15:38:14 -0800

    Added test to eliminate multiples of 100 from LYs

--- test_leapYear.py
+++ test_leapYear.py
@@ -4,8 +4,14 @@ import leapYear
 class testLeapYear (unittest.TestCase):
 
     # Test that all years divisible by 4 are leap years
-    def testLeapYear(self):
-        self.assertEqual(leapYear.leapYear(2000), True)
+    #def testLeapYearByFour(self):
+    #    self.assertEqual(leapYear.leapYear(2000), True)
+    #    self.assertEqual(leapYear.leapYear(2020), True)
+    #    self.assertEqual(leapYear.leapYear(2001), False)
+
+    # Test that all years divisible by 4 and not by 100 are leap years
+    def testLeapYearByFourAndHundred(self):
+        self.assertEqual(leapYear.leapYear(2000), False)
         self.assertEqual(leapYear.leapYear(2020), True)
         self.assertEqual(leapYear.leapYear(2001), False)
 
