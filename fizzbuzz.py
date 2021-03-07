d827d3a08f9362fff52a1118e86ffff831feffc1
Maddie Smith <smitmad9@oregonstate.edu>
2021-03-07 15:17:44 -0800

    Implemented "Buzz" replacement

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -1,5 +1,6 @@
 
-
+# Print all numbers 1-100 but replace multiples of 3 with "Fizz", multiples of
+# 5 with "Buzz", and multiples of both with "Fizzbuzz"
 def fb():
 
     result = ""
@@ -8,17 +9,26 @@ def fb():
 
     while i <= 100:
 
+        # Replace multiples of 3 with Fizz
         if i % 3 == 0:
             toAdd = "Fizz"
 
+        # Replace multiples of 5 with Buzz
+        elif i % 5 == 0:
+            toAdd = "Buzz"
+
+        # If not a multiple of 3 or 5, just keep the number i
         else:
             toAdd = str(i)
 
+        # Add the term to the result
         result += toAdd
 
+        # Add a space between each term for easy readability
         if i != 100:
             result += " "
 
+        # Go to the next term
         i += 1
 
     print(result)
