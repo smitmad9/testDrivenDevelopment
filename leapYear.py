b8f9cdbfe3d7a13dc9354b1a4dd6d5c91a53524a
Maddie Smith <smitmad9@oregonstate.edu>
2021-03-07 15:41:10 -0800

    Implemented excluding multiples of 100 from LYs

--- leapYear.py
+++ leapYear.py
@@ -1,9 +1,12 @@
 
-
+# Given a year, return whether it was a leap year
 def leapYear(n):
 
+    # If the year is divisible by 4, it is a leap year
     if n % 4 == 0:
-        return True
 
-    else:
-        return False
+        # The year must also NOT be divisible by 100 to be a leap year
+        if n % 100 != 0:
+            return True
+
+    return False
